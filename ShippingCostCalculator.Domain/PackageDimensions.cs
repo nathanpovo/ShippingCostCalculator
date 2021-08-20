97a48a7ecfd19c1278584f71f909b9496f78b1f1
Nathan Povo <17831160+nathanpovo@users.noreply.github.com>
2021-08-20 07:43:20 +0200

    Create PackageDimensions

---
+++ ShippingCostCalculator.Domain/PackageDimensions.cs
@@ -0,0 +1,16 @@
+namespace ShippingCostCalculator.Domain
+{
+    public record PackageDimensions
+    {
+        public PackageDimensions(float length, float width, float height)
+            => Volume = CalculateVolume(length, width, height);
+
+        public PackageDimensions(float volume)
+            => Volume = volume;
+
+        public float Volume { get; }
+
+        private static float CalculateVolume(float length, float width, float height)
+            => length * width * height;
+    }
+}
