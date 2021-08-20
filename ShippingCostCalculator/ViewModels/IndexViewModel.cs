a3f2ea86441e94d694a0c0fcc00682a4c2e1e119
Nathan Povo <17831160+nathanpovo@users.noreply.github.com>
2021-08-20 13:19:29 +0200

    Add ReactiveUI library

---
+++ ShippingCostCalculator/ViewModels/IndexViewModel.cs
@@ -0,0 +1,10 @@
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using ShippingCostCalculator.Domain;
+
+namespace ShippingCostCalculator.ViewModels
+{
+    public class IndexViewModel : ReactiveObject
+    {
+    }
+}
---
+++ ShippingCostCalculator/Views/Index.razor.cs
@@ -0,0 +1,12 @@
+using ShippingCostCalculator.ViewModels;
+
+namespace ShippingCostCalculator.Views
+{
+    public partial class Index
+    {
+        public Index()
+        {
+            ViewModel ??= new IndexViewModel();
+        }
+    }
+}
