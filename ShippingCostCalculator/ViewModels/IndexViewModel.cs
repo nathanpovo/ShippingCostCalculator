c07749db62dbc28386adae78a520417b1e7d7bb5
Nathan Povo <17831160+nathanpovo@users.noreply.github.com>
2021-08-22 12:55:02 +0200

    Add basic UI

--- ShippingCostCalculator/ViewModels/IndexViewModel.cs
+++ ShippingCostCalculator/ViewModels/IndexViewModel.cs
@@ -6,5 +6,26 @@ namespace ShippingCostCalculator.ViewModels
 {
     public class IndexViewModel : ReactiveObject
     {
+        public IndexViewModel()
+        {
+            this.WhenAnyValue(x => x.Length, x => x.Width, x => x.Height,
+                    (length, width, height) => new PackageDimensions(length, width, height))
+                .ToPropertyEx(this, x => x.PackageDimensions);
+        }
+
+        [Reactive]
+        public float Length { get; set; }
+
+        [Reactive]
+        public float Width { get; set; }
+
+        [Reactive]
+        public float Height { get; set; }
+
+        [Reactive]
+        public float Weight { get; set; }
+
+        [ObservableAsProperty]
+        public PackageDimensions? PackageDimensions { get; }
     }
 }
