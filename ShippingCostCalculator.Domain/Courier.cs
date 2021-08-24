5426d43156bc1168f4530cf597f8046fee197fe9
Nathan Povo <17831160+nathanpovo@users.noreply.github.com>
2021-08-24 18:09:11 +0200

    Add observables of validations and cost to Courier

--- ShippingCostCalculator.Domain/Courier.cs
+++ ShippingCostCalculator.Domain/Courier.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using ShippingCostCalculator.Domain.CostCalculators;
 using ShippingCostCalculator.Domain.PackageDetailsValidators;
 
@@ -8,6 +10,10 @@ namespace ShippingCostCalculator.Domain
     {
         internal readonly ICostCalculator CostCalculator;
         internal readonly IPackageDetailsValidator PackageDetailsValidator;
+
+        private readonly ReplaySubject<ValidationResult> volumeValidationSubject = new(1);
+        private readonly ReplaySubject<ValidationResult> weightValidationSubject = new(1);
+        private readonly ReplaySubject<float?> packageCostSubject = new(1);
 
         private Courier(ICostCalculator costCalculator, IPackageDetailsValidator packageDetailsValidator)
         {
@@ -30,6 +36,27 @@ namespace ShippingCostCalculator.Domain
         internal static Courier Create(ICostCalculator costCalculator, IPackageDetailsValidator packageDetailsValidator)
             => new(costCalculator, packageDetailsValidator);
 
+        public void InitializeObservables(IObservable<PackageDimensions> packageDimensionsObservable,
+            IObservable<float> weightObservable)
+            => packageDimensionsObservable
+                .CombineLatest(weightObservable, (packageDimensions, weight) =>
+                {
+                    ValidationResult volumeValidation = IsVolumeValid(packageDimensions);
+                    ValidationResult weightValidation = IsWeightValid(weight);
+
+                    volumeValidationSubject.OnNext(volumeValidation);
+                    weightValidationSubject.OnNext(weightValidation);
+
+                    return volumeValidation.IsValid && weightValidation.IsValid
+                        ? CalculateCost(packageDimensions, weight)
+                        : (long?) null;
+                })
+                .Select(cost
+                    => cost is not null
+                        ? (float) cost / 1000f
+                        : (float?) null)
+                .Subscribe(packageCostSubject);
+
         public long CalculateCost(PackageDimensions packageDimensions, float weight)
         {
             long costBasedOnDimensions = CostCalculator.CalculateCostBasedOnDimensions(packageDimensions);
@@ -39,6 +66,10 @@ namespace ShippingCostCalculator.Domain
                 ? costBasedOnDimensions
                 : costBasedOnWeight;
         }
+
+        public IObservable<float?> PackageCost => packageCostSubject.AsObservable();
+        public IObservable<ValidationResult> VolumeValidation => volumeValidationSubject.AsObservable();
+        public IObservable<ValidationResult> WeightValidation => weightValidationSubject.AsObservable();
 
         /// <inheritdoc cref="IPackageDetailsValidator.IsVolumeValid"/>
         public ValidationResult IsVolumeValid(PackageDimensions packageDimensions)
