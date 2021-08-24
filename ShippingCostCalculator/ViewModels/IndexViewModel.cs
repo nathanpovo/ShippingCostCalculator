822335389f060ae9f511198eff4bea034727db01
Nathan Povo <17831160+nathanpovo@users.noreply.github.com>
2021-08-24 19:30:37 +0200

    Set and display package details validation message

--- ShippingCostCalculator/ViewModels/IndexViewModel.cs
+++ ShippingCostCalculator/ViewModels/IndexViewModel.cs
@@ -4,6 +4,7 @@ using System.Reactive.Linq;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 using ShippingCostCalculator.Domain;
+using ShippingCostCalculator.Domain.PackageDetailsValidators;
 
 namespace ShippingCostCalculator.ViewModels
 {
@@ -35,12 +36,61 @@ namespace ShippingCostCalculator.ViewModels
             SetupCostProperty(cargo4You, x => x.Cargo4YouCost);
             SetupCostProperty(shipFaster, x => x.ShipFasterCost);
             SetupCostProperty(maltaShip, x => x.MaltaShipCost);
+
+            SetupValidationProperty(cargo4You, x => x.Cargo4YouValidation);
+            SetupValidationProperty(shipFaster, x => x.ShipFasterValidation);
+            SetupValidationProperty(maltaShip, x => x.MaltaShipValidation);
         }
 
         private void SetupCostProperty(Courier courier, Expression<Func<IndexViewModel, string?>> property)
             => courier.PackageCost
                     .Select(cost => cost is not null ? $"€{cost.Value:F3}" : null)
                     .ToPropertyEx(this, property);
+
+        private void SetupValidationProperty(Courier courier, Expression<Func<IndexViewModel, string?>> property)
+        {
+            IObservable<string?> weightMessageObservable = courier
+                .WeightValidation
+                .Select(validationResult => BuildValidationMessage(validationResult, "weight"));
+
+            IObservable<string?> volumeMessageObservable = courier
+                .VolumeValidation
+                .Select(validationResult => BuildValidationMessage(validationResult, "volume"));
+
+            volumeMessageObservable
+                .CombineLatest(weightMessageObservable, (volumeMessage, weightMessage)
+                        => volumeMessage is not null && weightMessage is not null
+                            ? $"For this courier the {weightMessage} and the {volumeMessage}."
+                            : volumeMessage is not null
+                                ? $"For this courier the {volumeMessage}."
+                                : weightMessage is not null
+                                    ? $"For this courier the {weightMessage}."
+                                    : null)
+                .ToPropertyEx(this, property);
+        }
+
+        private static string? BuildValidationMessage(ValidationResult validationResult, string validationType)
+        {
+            if (validationResult.IsValid || validationResult.ValidationError is null)
+            {
+                return null;
+            }
+
+            ValidationLimits? limits = validationResult.ValidationError.ValidationLimits;
+
+            return validationResult.ValidationError.ErrorType switch
+            {
+                ValidationErrorType.ValueIsTooHigh => limits?.UpperLimit is not null
+                    ? $"{validationType} must be less than {limits.UpperLimit}"
+                    : $"{validationType} is too high",
+                ValidationErrorType.ValueIsTooLow => limits?.LowerLimit is not null
+                    ? $"{validationType} must be greater than {limits.LowerLimit}"
+                    : $"{validationType} is too low",
+                _ => limits?.UpperLimit is not null && limits.LowerLimit is not null
+                    ? $"{validationType} must be between {limits.LowerLimit} and {limits.UpperLimit}"
+                    : $"{validationType} is not within range"
+            };
+        }
 
         private static Courier CreateCourier(CourierType courierType,
             IObservable<PackageDimensions> packageDimensionsObservable, IObservable<float> weightObservable)
@@ -74,5 +124,14 @@ namespace ShippingCostCalculator.ViewModels
 
         [ObservableAsProperty]
         public string? MaltaShipCost { get; }
+
+        [ObservableAsProperty]
+        public string? Cargo4YouValidation { get; }
+
+        [ObservableAsProperty]
+        public string? ShipFasterValidation { get; }
+
+        [ObservableAsProperty]
+        public string? MaltaShipValidation { get; }
     }
 }
